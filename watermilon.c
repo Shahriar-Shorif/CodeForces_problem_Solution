eff450926a41e44eaeb42cc1f3a239f17cdcfa86
Shoriful Islam <85014035+Shahriar-Shorif@users.noreply.github.com>
2023-05-20 13:55:00 +0600

    Add files via upload

--- watermilon.c
+++ watermilon.c
@@ -3,11 +3,14 @@ int main()
 {
     int n;
     scanf("%d",&n);
-    if(n%2 == 0 && n>=2){
-        printf("YES\n");
-    }else
-    {
-        printf("NO\n");
+    if(n <= 100 && n>=1){
+            if(n == 2){
+                printf("NO\n");
+            }else if(n%2 == 0 ){
+                printf("YES\n");
+            }else{
+                printf("NO\n");
+            }
     }
 
 
